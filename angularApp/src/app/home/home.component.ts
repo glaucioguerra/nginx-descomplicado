22e2e430869795b60ff625977a75a97b9ec6098d
Glaucio Guerra <glaucio.guerra@markway.com.br>
2023-03-17 15:32:02 -0300

    adicionado app angular demo

---
+++ angularApp/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <h1>Bem-vindo à minha aplicação Angular!</h1>
+    <p>Esta é a página inicial.</p>
+  `,
+})
+export class HomeComponent {}
+
